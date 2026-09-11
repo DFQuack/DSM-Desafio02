9eaf6f05bc0d75c63e8992589a58d5d27d3be314
Daniel <dfquack08@gmail.com>
2026-09-10 19:17:38 -0600

    update: edit name of destination activity

--- app/src/main/java/sv/edu/udb/desafio02/EditDestinationActivity.kt
+++ app/src/main/java/sv/edu/udb/desafio02/EditDestinationActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class AddDestinationActivity : AppCompatActivity() {
+class EditDestinationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_destination)
+        setContentView(R.layout.activity_edit_destination)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
