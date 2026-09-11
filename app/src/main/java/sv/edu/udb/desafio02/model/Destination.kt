2a66dffbbe82eecbcb4e777880943bfb4f3b711b
Daniel <dfquack08@gmail.com>
2026-09-11 12:09:11 -0600

    add: create Destination class

---
+++ app/src/main/java/sv/edu/udb/desafio02/model/Destination.kt
@@ -0,0 +1,10 @@
+package sv.edu.udb.desafio02.model
+
+data class Destination(
+    var id: String = "",
+    var name: String = "",
+    var country: String = "",
+    var price: Double = 0.0,
+    var description: String = "",
+    var imageBase64: String? = null
+)
